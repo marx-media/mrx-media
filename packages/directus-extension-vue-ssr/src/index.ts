3af2e78967838c92f651d82d150d2100508133b8
MARX Dominic <dominic.marx@axians.de>
2022-05-20 10:54:41 +0200

    fix: eslint

--- packages/directus-extension-vue-ssr/src/index.ts
+++ packages/directus-extension-vue-ssr/src/index.ts
@@ -4,8 +4,8 @@ import { defineHook } from '@directus/extensions-sdk';
 import { expressMiddleware } from '@mrx-media/vite-vue-simple-ssr/server';
 
 export default defineHook(({ init }) => {
-  init('routes.custom.before', async ({ app }) => {
-    const root = dirname(fileURLToPath(import.meta.url));
-    await expressMiddleware(app, { root });
-  });
+	init('routes.custom.before', async ({ app }) => {
+		const root = dirname(fileURLToPath(import.meta!.url));
+		await expressMiddleware(app, { root });
+	});
 });
